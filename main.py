8b0395accba2f33e2251167315f583843d2990f5
Artem Pleshakov <tema.plesk2007@gmail.com>
2026-02-10 01:01:52 +0300

    add Bubble sort

--- main.py
+++ main.py
@@ -11,9 +11,17 @@ class Sort:
     
     def get_info(self):
         return self.main_array
-    
-first_array = Sort(50,1000)
-first_array.create_array()
-print(first_array.get_info())
-        
 
+class Bubble_sort(Sort):
+    def sorting(self):
+        n = len(self.main_array)
+        for i in range(n - 1):
+            for j in range(n - i - 1):
+                if self.main_array[j] > self.main_array[j + 1]:
+                    self.main_array[j],self.main_array[j + 1] = self.main_array[j + 1], self.main_array[j]
+        return self.main_array
+array = Bubble_sort(10,100)
+array.create_array()
+print(array.get_info())
+array.sorting()
+print(array.get_info())                
